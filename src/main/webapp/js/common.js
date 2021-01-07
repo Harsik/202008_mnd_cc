be70c28fa0b6504556b85cf57769ac8f94624bfa
hshong <hshong@tiflis.co.kr>
2021-01-07 07:06:51 +0000

    상담이력 전화번호 정규식 공통함수로 뺐음

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -560,3 +560,29 @@ function fnConvertDateFormat(dateData) {
 	
 	return dateString;
 }
+
+
+/**
+ * 전화번호 포맷
+ */
+function telnoFormat(num){
+	var formatNum = '';
+
+    if(num.length>11){
+        formatNum = num;
+    }else if(num.length==11){
+    	formatNum = num.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3');
+    }else if(num.length==8){
+        formatNum = num.replace(/(\d{4})(\d{4})/, '$1-$2');
+    }else if(num.length==7){
+        formatNum = num.replace(/(\d{3})(\d{4})/, '$1-$2');
+    }else{
+        if(num.indexOf('02')==0){
+        	formatNum = num.replace(/(\d{2})(\d{4})(\d{4})/, '$1-$2-$3');
+        }else{
+            formatNum = num.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+        }
+    }
+
+    return formatNum;
+}
